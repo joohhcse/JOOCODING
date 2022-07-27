7e0f9fc2b1642b71787f58ba5b1e0a960ebd2446
joohhcse <joohhcse@gmail.com>
2022-07-27 22:58:12 +0900

    숫자문자열과영단어

---
+++ lv1_숫자문자열과영단어.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String s) {
+        String[] nums = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] words = {
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"
+        };
+        
+        for(int i = 0 ; i < words.length ; i++) {
+            s = s.replace(words[i], nums[i]);
+        }
+        
+        return Integer.parseInt(s);
+    }
+}
